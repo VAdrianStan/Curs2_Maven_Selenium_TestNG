f39f2a8e94f4efe29b9b871996d5ef57efe66407
Stan <victoradrian.stan@gmail.com>
2024-01-18 21:02:41 +0200

    Part II- a

---
+++ src/main/java/ErrorMessage.java
@@ -0,0 +1,7 @@
+public class ErrorMessage {
+
+    public static final String TITLE_IS_NOT_DISPLAYED = "The title is not displayed!";
+    public static final String TITLE_COLOR_ERROR = "The title color is not correct";
+    public static final String CART_COLOR_ERROR= "The cart color is wrong!";
+
+}
--- src/main/java/TestData.java
+++ src/main/java/TestData.java
@@ -2,6 +2,10 @@ import java.util.Random;
 
 public class TestData {
 
+    public static final String URL = "https://tutorialsninja.com/demo/";
+    public static final String BLUE_COLOR = "rgba(35, 161, 209, 1)";
+    public static final String BLACK_COLOR = "rgba(54, 54, 54, 1)";
+
     public static String generateNewEmail() {
 
        Random random = new Random();
